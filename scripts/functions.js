446bc2d36171ff8e19e12296627b42dcd2e26a08
John <megaman5249@gmail.com>
2021-11-27 18:28:01 -0500

    Add evaluate function, negate function, and rules around decimal

--- scripts/functions.js
+++ scripts/functions.js
@@ -19,36 +19,100 @@ function addToDisplay(char){
 };
 
 function evaluate(string){
-    
+    let round = 100000
+    if(string.includes("/")){
+        let splits = string.split("/");
+        let top = splits["0"];
+        let bottom = splits["1"];
+        if (!isNaN(top) && !isNaN(bottom)){
+            return Math.round((top/bottom)*round)/round;
+        };
+    } else if (string.includes("*")){
+        let splits = string.split("*");
+        let top = splits["0"];
+        let bottom = splits["1"];
+        if (!isNaN(top) && !isNaN(bottom)){
+            return Math.round((top*bottom)*round)/round;
+        };
+    } else if (string.includes("+")){
+        let splits = string.split("+");
+        let top = splits["0"];
+        let bottom = splits["1"];
+        if (!isNaN(top) && !isNaN(bottom)){
+            return Math.round((top+bottom)*round)/round;
+        };
+    } else if (string.includes("-")){
+        let splits = string.split("-");
+        let top = splits["0"];
+        let bottom = splits["1"];
+        if (!isNaN(top) && !isNaN(bottom)){
+            return Math.round((top-bottom)*round)/round;
+        };
+    };
+
 };
 
 let numbers = Array.from(document.querySelectorAll(".number"));
 numbers.forEach((element)=>{
-    element.addEventListener("click", (e)=>{
-        addToDisplay(e.target.innerText);
-    });
+    if (element.id=="point"){
+        element.addEventListener("click", (e)=>{
+            if (display.innerText == ""){
+                display.innerText = "0.";
+            }else if (!display.innerText.includes(".")){
+                addToDisplay(e.target.innerText);
+            };
+        });
+    }else{
+        element.addEventListener("click", (e)=>{
+            addToDisplay(e.target.innerText);
+        });
+    };
 });
 
-document.getElementById("clear").addEventListener("click", (e)=>{
-    display.innerText = null;
+document.getElementById("clear").addEventListener("click", ()=>{
+    display.innerText = "";
 });
 
-document.getElementById("delete").addEventListener("click", (e)=>{
-    if(display.innerText !== null){
+document.getElementById("delete").addEventListener("click", ()=>{
+    if(display.innerText !== ""){
         display.innerText = display.innerText.slice(0,-1);
     };
 });
 
-let operators = Array.from(document.querySelectorAll(".operator"));
-operators.forEach((element)=>{
-    element.addEventListener("click", (e)=>{
-        if(
+document.getElementById("neg").addEventListener("click", ()=>{
+    if(
         display.innerText.includes("/")||
         display.innerText.includes("*")||
         display.innerText.includes("+")||
         display.innerText.includes("-")){
             display.innerText = evaluate(display.innerText);
-        }
-        addToDisplay(e.target.dataset.key);
-    });
+        };
+    display.innerText = evaluate(`-1*${display.innerText}`);
+});
+
+let operators = Array.from(document.querySelectorAll(".operator"));
+operators.forEach((element)=>{
+    if (element.id == "equal"){
+        element.addEventListener("click", ()=>{
+            if(
+                display.innerText.includes("/")||
+                display.innerText.includes("*")||
+                display.innerText.includes("+")||
+                display.innerText.includes("-")){
+                    display.innerText = evaluate(display.innerText);
+                };
+        });
+
+    } else {
+        element.addEventListener("click", (e)=>{
+            if(
+                display.innerText.includes("/")||
+                display.innerText.includes("*")||
+                display.innerText.includes("+")||
+                display.innerText.includes("-")){
+                    display.innerText = evaluate(display.innerText);
+                };
+            addToDisplay(e.target.dataset.key);
+        });
+    };
 });
